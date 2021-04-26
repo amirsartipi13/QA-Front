19a6c42bf167c4adf0396a54d61c8e3eb7bf11ef
sart sar <amirsartipi13@gmail.com>
2021-04-26 16:06:24 +0430

    add search input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './home/navbar/navbar.component'
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { DescriptionComponent } from './home/description/description.component';
+import { SearchComponent } from './home/search/search.component'
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    DescriptionComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
